9dc7e0c9fcbd8e2afecf8e6480205269cbd73e30
Jahna Arielle Goldman <jagoldman@uchicago.edu>
2017-01-30 00:52:18 -0600

    added pythonanywhere as allowed host

--- petproject/settings.py
+++ petproject/settings.py
@@ -31,7 +31,7 @@ STATICFILES_DIRS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'jahnagoldman.pythonanywhere.com', 'localhost']
 
 # Application definition
 
